644930b58238a8240fe99d0ee37b10fea3873c7a
HandriAthallahS <handriathallah@gmail.com>
2026-04-29 06:59:41 +0700

    update konfigurasi database dan perbaiki query user controller

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getProfile, updateProfile } = require('../controllers/user');
+
+router.get('/:id', getProfile);
+router.put('/:id', updateProfile);
+
+module.exports = router;
